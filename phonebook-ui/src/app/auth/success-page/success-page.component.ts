ee09abc38ec7ee292acaed1457cdbd8d880ca324
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-06-11 17:41:42 +0200

    adds send token method and output results to html

--- phonebook-ui/src/app/auth/success-page/success-page.component.ts
+++ phonebook-ui/src/app/auth/success-page/success-page.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {UserService} from "../../../service/user.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-success-page',
   templateUrl: './success-page.component.html',
   styleUrls: ['./success-page.component.css']
 })
-export class SuccessPageComponent implements OnInit {
+export class SuccessPageComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,
+              private userService: UserService) {
   }
 
+  errorStat: boolean;
+  confirmMessage: string;
+
+  ngOnInit(): void {
+    this.sendToken()
+  }
+
+  sendToken() {
+    const token = this.route.snapshot.paramMap.get('token');
+
+    const messageSuccess = 'Your email was successfully confirmed!';
+    const messageError = 'Your email cannot be verified';
+
+    this.subscription = this.userService.sendToken(token).subscribe(value => {
+        this.errorStat = false;
+        this.confirmMessage = messageSuccess;
+      },
+      error => {
+        this.errorStat = true;
+        this.confirmMessage = messageError;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
