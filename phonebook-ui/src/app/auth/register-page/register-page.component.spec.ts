5201e9091b785dc4525d43028ce27498f71e940e
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-06-11 17:40:15 +0200

    adds registration form, validators and tests

--- phonebook-ui/src/app/auth/register-page/register-page.component.spec.ts
+++ phonebook-ui/src/app/auth/register-page/register-page.component.spec.ts
@@ -1,25 +1,152 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { RegisterPageComponent } from './register-page.component';
+import {RegisterPageComponent} from './register-page.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {UserService} from "../../../service/user.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Observable} from "rxjs";
+import {User} from "../../common/model/user";
 
-describe('RegisterPageComponent', () => {
+describe('Component: Login', () => {
+
   let component: RegisterPageComponent;
   let fixture: ComponentFixture<RegisterPageComponent>;
+  let userServiceStub: Partial<UserService>;
+  let userService;
+  // httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+  userServiceStub = {
+    register(user: User): Observable<User> {
+      user.email = 'mail.user@mail.com';
+      user.password = 'passuser1';
+      return;
+    }
+  };
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ RegisterPageComponent ]
-    })
-    .compileComponents();
-  }));
+  const fakeActivatedRoute = {
+    snapshot: {
+      queryParams: {
+        returnUrl: '/'
+      }
+    }
+  };
+  const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+
+  // userService: UserService = fixture.debugElement.injector.get(UserService);
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, FormsModule, HttpClientTestingModule],
+      declarations: [RegisterPageComponent],
+      providers: [
+        {provide: UserService, useValue: userServiceStub},
+        {provide: Router, useValue: routerSpy},
+        {provide: ActivatedRoute, useFactory: () => fakeActivatedRoute}
+      ]
+    });
+
+    // create component and test fixture
     fixture = TestBed.createComponent(RegisterPageComponent);
+
+    // get test component from the fixture
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    // UserService from the root injector
+    userService = TestBed.inject(UserService);
+
+
+    component.ngOnInit();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+
+  it('form empty. form invalid', () => {
+    expect(component.form.valid).toBeFalsy();
+  });
+
+  it('password field minlength 5, password.minlength. error and form invalid ', () => {
+    let errors = {};
+    let password = component.form.controls['password'];
+
+    password.setValue("123456");
+    errors = password.errors || {};
+    expect(errors['minlength']).toBeTruthy();
+    expect(component.form.valid).toBeFalsy();
+  });
+
+  it('password field maxlength 30. password. maxlength.error and form invalid', () => {
+    let errors = {};
+    let password = component.form.controls['password'];
+
+    password.setValue("123456789012345678901234567890");
+    errors = password.errors || {};
+    expect(errors['maxlength']).toBeTruthy();
+    expect(component.form.valid).toBeFalsy();
+  });
+
+  it('password field empty, email valid. email valid and form invalid', () => {
+    let errors = {};
+    let password = component.form.controls['password'];
+    let email = component.form.controls['email'];
+
+    email.setValue("email@valid.ok")
+    password.setValue("");
+    errors = password.errors || {};
+    expect(errors['required']).toBeTruthy();
+    expect(component.form.valid).toBeFalsy();
+  });
+
+  it('password confirm not equals password, email valid. email valid and form invalid', () => {
+    let password = component.form.controls['password'];
+    let passwordConfirm = component.form.controls['passwordConfirm'];
+    let email = component.form.controls['email'];
+
+    email.setValue("email@valid.ok")
+    password.setValue("1234567890");
+    passwordConfirm.setValue("12345678901");
+
+    expect(component.form.valid).toBeFalsy();
+  });
+
+  it('password confirm equals password, email valid. email valid and form valid', () => {
+    let password = component.form.controls['password'];
+    let passwordConfirm = component.form.controls['passwordConfirm'];
+    let email = component.form.controls['email'];
+
+    email.setValue("email@valid.ok")
+    password.setValue("12345678901");
+    passwordConfirm.setValue("12345678901");
+
+    expect(component.form.valid).toBeTruthy();
+  });
+
+  // it('email ("email@valid") invalid. form invalid', () => {
+  //   let errors = {};
+  //   let password = component.form.controls['password'];
+  //   let passwordConfirm = component.form.controls['passwordConfirm'];
+  //   let email = component.form.controls['email'];
+  //
+  //   email.setValue("email@valid")
+  //   password.setValue("12345678901");
+  //   passwordConfirm.setValue("12345678901");
+  //
+  //   expect(errors['email']).toBeTruthy();
+  //   expect(component.form.valid).toBeFalsy();
+  // });
+
+  it('on submit', () => {
+    let password = component.form.controls['password'];
+    let passwordConfirm = component.form.controls['passwordConfirm'];
+    let email = component.form.controls['email'];
+
+    email.setValue("email@valid.ok")
+    password.setValue("12345678901");
+    passwordConfirm.setValue("1234567890");
+    component.loading = false;
+    component.onSubmit()
+
+    expect(component.submitted).toBeTrue();
+    expect(component.form.valid).toBeFalsy();
+    expect(component.loading).toBeFalse();
   });
 });
--- phonebook-ui/src/app/auth/register-page/register-page.component.ts
+++ phonebook-ui/src/app/auth/register-page/register-page.component.ts
@@ -1,27 +1,86 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {AuthMessages} from "../../common/messages/auth-messages";
+import {User} from "../../common/model/user";
+import {UserService} from "../../../service/user.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Subscription} from "rxjs";
+
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.css']
 })
-export class RegisterPageComponent implements OnInit {
+export class RegisterPageComponent implements OnInit, OnDestroy {
 
-  constructor(private authMessages: AuthMessages) { }
+  submitted: boolean;
+  loading: boolean;
+  errorMessage: string;
 
   form: FormGroup
-  message = this.authMessages
+
+  private userSubscription: Subscription;
+
+  constructor(private userService: UserService,
+              private router: Router,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      name: new FormControl(null, [Validators.required, Validators.minLength(1)]),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(8)])
-    })
+        email: new FormControl(null,
+          [
+            Validators.required,
+            Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9-]+\.[a-z]{2,4}$")
+          ]),
+        password: new FormControl(null, [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(20)
+        ]),
+        passwordConfirm: new FormControl(null, [
+          Validators.required,
+          Validators.minLength(8)
+        ])
+      }, {validators: this.checkPasswords}
+    );
   }
 
-  OnSubmit() {
+  get f() {
+    return this.form.controls;
+  }
 
+  onSubmit() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    this.loading = true;
+
+    let user: User = new User();
+    user.email = this.form.value.email;
+    user.password = this.form.value.password;
+
+    this.userSubscription = this.userService.register(user)
+      .subscribe(_ => this.router.navigate(['../confirmation'], {relativeTo: this.route}),
+        error => {
+          this.loading = false;
+          this.errorMessage = errorHandler(error);
+        });
+
+    function errorHandler(error: any): string {
+      return 'Error: ' + error.statusText;
+    }
+  }
+
+  checkPasswords(group: FormGroup) {
+    let pass = group.get('password').value;
+    let confirmPass = group.get('passwordConfirm').value;
+
+    return pass === confirmPass ? null : {notSame: true}
+  }
+
+  ngOnDestroy(): void {
+    if (this.userSubscription)
+      this.userSubscription.unsubscribe();
   }
 }
