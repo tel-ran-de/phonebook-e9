db50f0e1657f1439390a2b0b76e51e9a93fc7141
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-06-12 19:08:08 +0200

    adds routes

--- phonebook-ui/src/app/app-routing.module.ts
+++ phonebook-ui/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {RegisterComponent} from "./auth/register/register.component";
+import {ActivationComponent} from "./auth/activation/activation.component";
+import {PendingComponent} from "./auth/pending/pending.component";
+import {AuthLayoutComponent} from "./common/layout/auth-layout/auth-layout.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AuthLayoutComponent, children: [
+      // { path: '', redirectTo: '/login', pathMatch: 'full' },
+      {path: '', redirectTo: 'user/register', pathMatch: 'full'},
+      {path: 'user/register', component: RegisterComponent},
+      {path: 'user/activation', component: ActivationComponent},
+      {path: 'user/pending/:token', component: PendingComponent},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
