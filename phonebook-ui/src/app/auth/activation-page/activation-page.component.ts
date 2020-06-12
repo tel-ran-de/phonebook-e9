d8749ab7fc7a71e9633a7c1d1ba57d512a96d7f3
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-06-12 21:13:57 +0200

    renamed components

---
+++ phonebook-ui/src/app/auth/activation-page/activation-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmation',
+  templateUrl: './activation-page.component.html',
+  styleUrls: ['./activation-page.component.css']
+})
+export class ActivationPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
