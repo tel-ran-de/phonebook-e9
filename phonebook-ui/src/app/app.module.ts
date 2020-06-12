3644e492e464fe6ede7f9a228870201c333a079f
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-06-12 19:05:00 +0200

    adds components to imports and declarations

--- phonebook-ui/src/app/app.module.ts
+++ phonebook-ui/src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from "./app.component";
+import {RegisterComponent} from "./auth/register/register.component";
+import {ActivationComponent} from "./auth/activation/activation.component";
+import {PendingComponent} from "./auth/pending/pending.component";
+import {AppRoutingModule} from "./app-routing.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {AuthLayoutComponent} from "./common/layout/auth-layout/auth-layout.component";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthLayoutComponent,
+    RegisterComponent,
+    ActivationComponent,
+    PendingComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
