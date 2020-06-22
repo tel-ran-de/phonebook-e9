08503b076242312930a00e23d4d880861e677502
ingkhn <57235234+ingkhn@users.noreply.github.com>
2020-06-22 16:56:45 +0200

    adds flyway and annotation @DirtiesContext to tests (#13)

--- phonebook-api/src/test/java/com/telran/phonebookapi/persistence/IConfirmationTokenRepositoryTest.java
+++ phonebook-api/src/test/java/com/telran/phonebookapi/persistence/IConfirmationTokenRepositoryTest.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class IConfirmationTokenRepositoryTest {
 
     @Autowired
